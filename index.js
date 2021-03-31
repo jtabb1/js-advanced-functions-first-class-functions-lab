dfc4651904aaf7288a89f5775cea2e00357f4c19
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-03-31 12:47:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(drivers.length-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// first version:
+// function createFareMultiplier(n) {
+//     return function(fare) {
+//         return n * fare;
+//     }
+// }
+
+// second simplified version:
+// function createFareMultiplier(n) {
+//     return (fare) => n * fare
+// }
+
+// third and final simplified version:
+const createFareMultiplier = n => fare => n * fare;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers,select) => select(drivers);
+
